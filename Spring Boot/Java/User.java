c2e122bfe6b9ccd10adcf104cdcdfd771acf2edc
maadhudheeraj1999 <maadhudheeraj1999@gmail.com>
2026-01-19 10:00:51 -0500

    Create User.java

---
+++ Spring Boot/Java/User.java
@@ -0,0 +1,11 @@
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String role; // ADMIN / USER
+}
